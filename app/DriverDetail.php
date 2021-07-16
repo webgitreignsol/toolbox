335db06142175c9e39a868019c909be83ab37119
unknown <shahzaib.imran@reignsol.com>
2021-07-16 15:47:58 +0500

    vendor

--- app/DriverDetail.php
+++ app/DriverDetail.php
@@ -3,15 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+ use Illuminate\Contracts\Validation\Validator;
+ use Illuminate\Http\Exceptions\HttpResponseException;
 
 class DriverDetail extends Model
 {
-    protected $fillable = []
-'driver_name',
-'driver_contact',
-'photo',
-'photo',
-'car_make',
-'vehicle_registration_number',.
-
+	use LogsActivity;
+    protected $fillable = ['driver_contact', 'driver_photo', 'car_photo', 'car_make', 'car_registration_number', 'driver_id'];
+    public static $rules = ([
+   			'driver_contact' 			=> 'required',
+   			'driver_photo' 				=> 'required',
+   			'car_photo' 				=> 'required',
+   			'car_make' 					=> 'required',
+   			'car_registration_number' 	=> 'required'   			
+   			]);
+    public function driver()
+    {
+    	return $this->belongsTo('App\User', 'driver_id');
+    }
 }
