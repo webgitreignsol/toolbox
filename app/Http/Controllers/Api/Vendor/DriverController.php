dc7ce52c0268795903612dbe89d7faf10a125945
atif-backend101 <mr.daniel111@yahoo.com>
2021-07-29 21:27:17 +0500

    api update

--- app/Http/Controllers/Api/Vendor/DriverController.php
+++ app/Http/Controllers/Api/Vendor/DriverController.php
@@ -21,7 +21,7 @@ class DriverController extends Controller
 
    public function store(Request $request)
    {
-    $validator = Validator::make($request->all(), DriverDetail::$rules);
+      $validator = Validator::make($request->all(), DriverDetail::$rules);
 
       if ($validator->fails()) {
 
@@ -38,7 +38,7 @@ class DriverController extends Controller
    		$crimage->move(public_path('assets/admin/carImg'), $cr_img);
 
    		$auth_id = Auth::user()->id;
-
+      $driver = DriverDetail::where('driver_id', $auth_id)->first();
    		$arr = array(
      			'driver_contact' 			   => $request->driver_contact,
      			'driver_photo' 				   => $dr_img,
@@ -48,50 +48,20 @@ class DriverController extends Controller
      			'driver_id' 				     => Auth::user()->id
    			);
 
-      	DriverDetail::where('driver_id', $auth_id)->update($arr);
-
+      if ($driver == null) {
+      
+        DriverDetail::create($arr);
+      
+      }else{
+        DriverDetail::where('driver_id', $auth_id)->update($arr);
+        
+      }
       	return response()->json([
           'message'     => 'Success',
           'status'      => 1,
           'data'        => $arr
         ]);
-   }
-
-   public function update(Request $request)
-   {
-      if ($validator->fails()) {
-
-          return response()->json(['errors'=>$validator->errors()]);
-      }
-
-   		$drimage = $request->file('driver_photo');
-   		$crimage = $request->file('car_photo');
-
-   		$dr_img = rand().'.'. $drimage->getClientOriginalExtension();
-   		$drimage->move(public_path('assets/admin/driverImg'), $dr_img);
-
-   		$cr_img = rand().'.'. $crimage->getClientOriginalExtension();
-   		$crimage->move(public_path('assets/admin/carImg'), $cr_img);
-
-   		$auth_id = Auth::user()->id;
-
-   		$arr = array(
-     			'driver_contact' 			    => $request->driver_contact,
-     			'driver_photo' 				    => $dr_img,
-     			'car_photo' 				      => $cr_img,
-     			'car_make' 					      => $request->car_make,
-     			'car_registration_number' => $request->car_registration_number,
-     			'driver_id' 				      => $auth_id
-   			);
-
-      	DriverDetail::where('driver_id', $auth_id)->update($arr);
-
-      	return response()->json([
-          'message'       => 'Success',
-          'status'        => 1,
-          'data'          => $arr
-        ]);
-      }
+    }
 
     public function getAlltrips(Request $request)
     {
